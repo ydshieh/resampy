bfd788dd60cdf4c94d6d0e33de57d58685570f72
ydshieh <ydshieh@users.noreply.github.com>
2022-07-18 18:42:34 +0200

    test 2

--- resampy/core.py
+++ resampy/core.py
@@ -131,7 +131,17 @@ def resample(
     t_out = np.arange(shape[axis]) * time_increment
 
     # if parallel:
-    resample_f_p(
+    # resample_f_p(
+    #     x.swapaxes(-1, axis),
+    #     t_out,
+    #     interp_win,
+    #     interp_delta,
+    #     precision,
+    #     scale,
+    #     y.swapaxes(-1, axis),
+    # )
+    # else:
+    resample_f_s(
         x.swapaxes(-1, axis),
         t_out,
         interp_win,
@@ -140,16 +150,6 @@ def resample(
         scale,
         y.swapaxes(-1, axis),
     )
-    # else:
-    #     resample_f_s(
-    #         x.swapaxes(-1, axis),
-    #         t_out,
-    #         interp_win,
-    #         interp_delta,
-    #         precision,
-    #         scale,
-    #         y.swapaxes(-1, axis),
-    #     )
 
     return y
 
@@ -250,7 +250,17 @@ def resample_nu(
         t_out = t_out * sr_orig
 
     # if parallel:
-    resample_f_p(
+    # resample_f_p(
+    #     x.swapaxes(-1, axis),
+    #     t_out,
+    #     interp_win,
+    #     interp_delta,
+    #     precision,
+    #     1.0,
+    #     y.swapaxes(-1, axis),
+    # )
+    # else:
+    resample_f_s(
         x.swapaxes(-1, axis),
         t_out,
         interp_win,
@@ -259,15 +269,5 @@ def resample_nu(
         1.0,
         y.swapaxes(-1, axis),
     )
-    # else:
-    #     resample_f_s(
-    #         x.swapaxes(-1, axis),
-    #         t_out,
-    #         interp_win,
-    #         interp_delta,
-    #         precision,
-    #         1.0,
-    #         y.swapaxes(-1, axis),
-    #     )
 
     return y
